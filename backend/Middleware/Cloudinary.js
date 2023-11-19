c1058c56bfa2906db512a162821680e5640ff7dd
Deepak ts <tsdeepak589@gmail.com>
2023-11-19 13:13:14 +0530

    env added

--- backend/Middleware/Cloudinary.js
+++ backend/Middleware/Cloudinary.js
@@ -2,9 +2,9 @@ require("dotenv").config()
 const cloudinary = require('cloudinary').v2;
 
  cloudinary.config({
-  cloud_name: process.env.your_cloud_name,
-  api_key: process.env.your_api_key,
-  api_secret: process.env.your_api_secret
+  cloud_name: process.env.yourCloudName,
+  api_key: process.env.yourApiKey,
+  api_secret: process.env.yourApiSecret
 });
 
 module.exports = cloudinary
--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,9 +1,10 @@
 const mongoose=require('mongoose')
+require("dotenv").config()
 
 
 // mongoose.connect('mongodb://127.0.0.1:27017/poduppil').then(()=>{
 //     console.log("DB connected");
-mongoose.connect('mongodb+srv://DEEPAKTS:Tsdeepak589@loodieefasion.s7obwlv.mongodb.net/?retryWrites=true&w=majority').then(()=>{
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING).then(()=>{
     console.log("DB connected");
 })
 .catch(()=>{
